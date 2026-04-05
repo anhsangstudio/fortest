f9ee4c8eb26ca4e80f93c6bb74e5ff53ed84cdef
anhsangstudio <anhsangstudio.dl@gmail.com>
2026-04-05 15:25:37 +0700

    Create PhotoIDManager.tsx

---
+++ components/PhotoIDManager.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PhotoIDManager: React.FC = () => {
+  return (
+    <div>
+      <h2>Module Ảnh thẻ</h2>
+      <p>Đây là khu vực quản lý ảnh thẻ.</p>
+    </div>
+  );
+};
+
+export default PhotoIDManager;
